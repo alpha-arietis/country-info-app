555fbbdefcafda3e04d07d5a1d04ec808ff748b1
alpha-arietis <58612800+alpha-arietis@users.noreply.github.com>
2019-12-07 14:39:10 +1300

    Updates

---
+++ precache-manifest.7d719f1036658dc1974d1e9fddfe9a41.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7019880599bfcc0470319c9c63a50b38",
+    "url": "/country-info-app/index.html"
+  },
+  {
+    "revision": "3ccac1749cddb382dbbe",
+    "url": "/country-info-app/static/css/main.f20e553c.chunk.css"
+  },
+  {
+    "revision": "f2c65d311d848bc8888f",
+    "url": "/country-info-app/static/js/2.58a1bd5a.chunk.js"
+  },
+  {
+    "revision": "3ccac1749cddb382dbbe",
+    "url": "/country-info-app/static/js/main.4cec73fe.chunk.js"
+  },
+  {
+    "revision": "f426304f040bf9552397",
+    "url": "/country-info-app/static/js/runtime-main.28cf9c39.js"
+  },
+  {
+    "revision": "143be03f76743a6a42c115490fcdf6dc",
+    "url": "/country-info-app/static/media/area.143be03f.png"
+  },
+  {
+    "revision": "1d247e6c3654d086f0056650fd2b0943",
+    "url": "/country-info-app/static/media/capital.1d247e6c.png"
+  },
+  {
+    "revision": "d07e8961e9dcbb3566a52cfaebe72573",
+    "url": "/country-info-app/static/media/currencies.d07e8961.png"
+  },
+  {
+    "revision": "5c1f76718f0d9df91dc69b95ce84f0ba",
+    "url": "/country-info-app/static/media/glober-logo.5c1f7671.png"
+  },
+  {
+    "revision": "6f735e30eb37738e9ca57503b609a0c2",
+    "url": "/country-info-app/static/media/languages.6f735e30.png"
+  },
+  {
+    "revision": "476d10789bcae458a758cebe57eebc16",
+    "url": "/country-info-app/static/media/location.476d1078.png"
+  },
+  {
+    "revision": "facf499eb508d3111b9f2f2ed414aa4c",
+    "url": "/country-info-app/static/media/population.facf499e.png"
+  }
+]);
